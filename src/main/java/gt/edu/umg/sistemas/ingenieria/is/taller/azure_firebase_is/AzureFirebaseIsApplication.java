db6c8e64cc99f01ff31fc22ee818e6a30be82640
FernandoSicay <fsicayc@miumg.edu.gt>
2021-06-02 02:10:45 -0600

    project created

---
+++ src/main/java/gt/edu/umg/sistemas/ingenieria/is/taller/azure_firebase_is/AzureFirebaseIsApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.sistemas.ingenieria.is.taller.azure_firebase_is;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFirebaseIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureFirebaseIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gt/edu/umg/sistemas/ingenieria/is/taller/azure_firebase_is/AzureFirebaseIsApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.sistemas.ingenieria.is.taller.azure_firebase_is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureFirebaseIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
